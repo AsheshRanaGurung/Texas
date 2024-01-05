8b3b05527e876c9caab55796f98366a37092c55d
AsheshRanaGurung <ashesh7272@gmail.com>
2024-01-05 08:52:54 +0545

    feat pushed routes

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Text, Button, Card, CardBody, Image, Stack, Heading, CardFooter } from '@chakra-ui/react'
+import { Text, Button, Card, CardBody, Image, Stack, Heading, CardFooter, Modal, ModalBody, ModalContent, ModalHeader, ModalCloseButton, ModalFooter, ModalOverlay, useDisclosure } from '@chakra-ui/react'
 import axios from 'axios'
 import { BaseURL } from '../App'
 import { useParams } from "react-router-dom"
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify';
 const CardDetail = () => {
     const { id } = useParams()
     const [postDetail, setPostDetail] = useState({})
+    const { isOpen, onOpen, onClose } = useDisclosure()
     useEffect(() => {
 
         axios.get(`${BaseURL}/posts/${id}`).then(
@@ -22,34 +23,54 @@ const CardDetail = () => {
     }, [])
     console.log(postDetail)
     return (
-        <Card
-            direction={{ base: 'column', sm: 'row' }}
-            overflow='hidden'
-            variant='outline'
-        >
-            <Image
-                objectFit='cover'
-                maxW={{ base: '100%', sm: '200px' }}
-                src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                alt='Caffe Latte'
-            />
+        <>
 
-            <Stack>
-                <CardBody>
-                    <Heading size='md'>{postDetail?.title}</Heading>
+            <Card
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+            >
+                <Image
+                    objectFit='cover'
+                    maxW={{ base: '100%', sm: '200px' }}
+                    src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                    alt='Caffe Latte'
+                />
 
-                    <Text py='2'>
-                        {postDetail?.body}
-                    </Text>
-                </CardBody>
+                <Stack>
+                    <CardBody>
+                        <Heading size='md'>{postDetail?.title}</Heading>
 
-                <CardFooter>
-                    <Button variant='solid' colorScheme='blue'>
-                        Buy Latte
-                    </Button>
-                </CardFooter>
-            </Stack>
-        </Card>
+                        <Text py='2'>
+                            {postDetail?.body}
+                        </Text>
+                    </CardBody>
+
+                    <CardFooter>
+                        <Button variant='solid' colorScheme='blue' onClick={onOpen}>
+                            Edit
+                        </Button>
+                    </CardFooter>
+                </Stack>
+            </Card>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        this is modal
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
 
