4325edc82ef2805020702d2502832accf1782831
AsheshRanaGurung <ashesh7272@gmail.com>
2024-01-05 06:21:17 +0545

    feat: day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 import { useEffect, useState } from 'react'
-
-import { Text, Button, Input } from '@chakra-ui/react'
+import axios from "axios"
+import { Text, Button, Input, Flex, Box, Grid, GridItem } from '@chakra-ui/react'
 import './App.css'
 
+const BaseURL = "https://jsonplaceholder.typicode.com"
 function App() {
-    const BaseURL = "https://jsonplaceholder.typicode.com/posts"
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
-
+        axios.get(`${BaseURL}/posts`).then(
+            (res) => {
+                // console.log(res.data)
+                setPosts(res.data)
+            }
+        ).catch(
+            (err) => console.log(err)
+        )
     }, [])
     return (
         <>
+            <Grid templateColumns="repeat(3,1fr)">
+                {posts.map((post) => {
+                    return (
+                        <GridItem m={1} key={post.id}>
+                            <Box p={2} sx={{ border: "1px solid black" }}>
+                                <Text >
+                                    Title: {post.title}
+                                </Text>
+                                <Text>
+                                    Desc: {post.body}
+                                </Text>
+                            </Box>
+                        </GridItem>
 
+                    )
+                    // console.log(post.title)
+
+                })}
+            </Grid>
         </>
     )
 }
