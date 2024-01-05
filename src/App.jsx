5acd0379e73d4ab110382e7ed52601dfcb3de8a5
AsheshRanaGurung <ashesh7272@gmail.com>
2024-01-05 07:15:14 +0545

    feat: new card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import axios from "axios"
 import { Text, Button, Spinner, Center, Grid, GridItem, Card, CardBody, Image, Stack, Heading, Divider, CardFooter, ButtonGroup } from '@chakra-ui/react'
 import './App.css'
+import PostCard from './components/Cards'
 
 const BaseURL = "https://jsonplaceholder.typicode.com"
 function App() {
@@ -38,35 +39,7 @@ function App() {
                 {posts.map((post) => {
                     return (
                         <GridItem m={1} key={post.id}>
-                            <Card maxW='sm' height={"34.5rem"}>
-                                <CardBody>
-                                    <Image
-                                        src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                                        alt='Green double couch with wooden legs'
-                                        borderRadius='lg'
-                                    />
-                                    <Stack mt='6' spacing='3'>
-                                        <Heading size='md'>{post.title.slice(0, 30)}</Heading>
-                                        <Text>
-                                            {post.body.slice(0, 100)}
-                                        </Text>
-                                        <Text color='blue.600' fontSize='2xl'>
-                                            $450
-                                        </Text>
-                                    </Stack>
-                                </CardBody>
-                                <Divider />
-                                <CardFooter>
-                                    <ButtonGroup spacing='2'>
-                                        <Button variant='solid' colorScheme='blue'>
-                                            Buy now
-                                        </Button>
-                                        <Button variant='ghost' colorScheme='blue'>
-                                            Add to cart
-                                        </Button>
-                                    </ButtonGroup>
-                                </CardFooter>
-                            </Card>
+                            <PostCard title={post.title} desc={post.body} />
                         </GridItem>
 
                     )
---
+++ src/components/Cards.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Text, Button, Card as ChakraCard, CardBody, Image, Stack, Heading, Divider, CardFooter, ButtonGroup } from '@chakra-ui/react'
+
+const PostCard = ({ title, desc }) => {
+    return (
+        <ChakraCard maxW='sm' height={"34.5rem"}>
+            <CardBody>
+                <Image
+                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    alt='Green double couch with wooden legs'
+                    borderRadius='lg'
+                />
+                <Stack mt='6' spacing='3'>
+                    <Heading size='md'>{title.slice(0, 30)}</Heading>
+                    <Text>
+                        {desc.slice(0, 100)}
+                    </Text>
+                    <Text color='blue.600' fontSize='2xl'>
+                        $450
+                    </Text>
+                </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+                <ButtonGroup spacing='2'>
+                    <Button variant='solid' colorScheme='blue'>
+                        Buy now
+                    </Button>
+                    <Button variant='ghost' colorScheme='blue'>
+                        Add to cart
+                    </Button>
+                </ButtonGroup>
+            </CardFooter>
+        </ChakraCard>
+    )
+};
+
+export default PostCard;
