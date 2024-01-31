ca24a9cf4be685c96044337ebb5f00f5b8964441
AsheshRanaGurung <ashesh7272@gmail.com>
2024-01-31 15:20:04 +0545

    chore: added comment

--- src/main.jsx
+++ src/main.jsx
@@ -12,5 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             position="top-right" />
         <App />
     </ChakraProvider>
+    {/* this is a test comment. Please ignore */}
     </React.StrictMode>,
 )
