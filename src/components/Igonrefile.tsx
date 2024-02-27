cb17f2d8f7f99f64aa84cfb406e4dd127d5af127
AsheshRanaGurung <ashesh7272@gmail.com>
2024-02-27 16:46:27 +0545

    fix: and operator fix in service filter

--- src/components/Igonrefile.tsx
+++ src/components/Igonrefile.tsx
@@ -1,44 +1,100 @@
-import { Text } from '@chakra-ui/react';
-import { ServiceIcon } from 'icons';
-import React, { useEffect, useRef } from 'react';
-import { FILTER_TYPE_LABEL, OPERATOR_LABEL } from '../../../constant';
+ // serviceFilter: object().when('filterType', {
+  //   is: (filterGroup: FilterTypeOption[]) =>
+  //     filterGroup.map(f => f.value).includes(FilterTypes.SERVICE),
+  //   then: object().shape({
+  //     filters: object().shape({
+  //       operator: object().shape({
+  //         label: string().required('Operator label is required!'),
+  //         value: string()
+  //           .oneOf(Object.values(OperatorTypes), 'Invalid operator value')
+  //           .required('Operator value is required!'),
+  //       }),
+  //       entity: object()
+  //         .nullable(true)
+  //         .test('required', 'Service is required', function (value) {
+  //           return dropdownAvailableOptions.includes(this.parent.operator.value) ? !!value : true;
+  //         }),
+  //       value: string().test('required', 'Phrase is required', function (value) {
+  //         return !dropdownAvailableOptions.includes(this.parent.operator.value) ? !!value : true;
+  //       }),
+  //     }),
+  //   }),
+  //   otherwise: object().strip(),
+  // }),
+  // alertSourceFilter: object().when('filterType', {
+  //   is: (filterGroup: FilterTypeOption[]) => {
+  //     return filterGroup.map(f => f.value).includes(FilterTypes.ALERT_SOURCE);
+  //   },
+  //   then: object().shape({
+  //     filters: object().shape({
+  //       operator: object().shape({
+  //         label: string().required('Operator label is required!'),
+  //         value: string()
+  //           .oneOf(Object.values(OperatorTypes), 'Invalid operator value')
+  //           .required('Operator value is required!'),
+  //       }),
+  //       entity: object().test('required', 'Alert Source is required', function (value) {
+  //         return dropdownAvailableOptions.includes(this.parent.operator.value) ? !!value : true;
+  //       }),
+  //       value: string().test('required', 'Phrase is required', function (value) {
+  //         return !dropdownAvailableOptions.includes(this.parent.operator.value) ? !!value : true;
+  //       }),
+  //     }),
+  //   }),
+  //   otherwise: object().strip(),
+  // }),
+  // priorityFilter: object().when('filterType', {
+  //   is: (filterGroup: FilterTypeOption[]) => {
+  //     return filterGroup.map(f => f.value).includes(FilterTypes.PRIORITY);
+  //   },
+  //   then: object().shape({
+  //     filters: object().shape({
+  //       operator: object().shape({
+  //         label: string().required('Operator label is required!'),
+  //         value: string()
+  //           .oneOf(Object.values(OperatorTypes), 'Invalid operator value')
+  //           .required('Operator value is required!'),
+  //       }),
+  //       entity: object().test('required', 'Priority is required', function (value) {
+  //         return dropdownAvailableOptions.includes(this.parent.operator.value) ? !!value : true;
+  //       }),
+  //     }),
+  //   }),
+  //   otherwise: object().strip(),
+  // }),
+  // tagFilter: object().when('filterType', {
+  //   is: (filterGroup: FilterTypeOption[]) => {
+  //     return filterGroup.map(f => f.value).includes(FilterTypes.TAGS);
+  //   },
+  //   then: object().shape({
+  //     filters: array().of(
+  //       object().shape({
+  //         operator: object().shape({
+  //           label: string().required('Operator label is required!'),
+  //           value: string()
+  //             .oneOf(Object.values(OperatorTypes), 'Invalid operator value')
+  //             .required('Operator value is required!'),
+  //         }),
+  //         logicalOperator: object().shape({
+  //           label: string().required('Logical Operator label is required!'),
+  //           value: string().oneOf(
+  //             Object.values(LogicalOperatorTypes),
+  //             'Invalid Logical Operator value',
+  //           ),
+  //         }),
+  //         tag: object().shape({
+  //           key: object()
+  //             .shape({
+  //               label: string().required('Tag key is required!'),
+  //               value: string().required('Tag key is required!'),
+  //             })
+  //             .required('Tag key is required!'),
+  //           value: object().nullable(true).required('Tag value is required!'),
+  //         }),
+  //       }),
+  //     ),
+  //   }),
+  //   otherwise: object().strip(),
+  // }),
 
-import useServiceList from '../../../create-or-update/filter/service/useServiceList';
-import { OperatorTypes } from '../../../types/enums';
-import { safeEncodeUriComponent } from '../../../utils';
-import { Loader } from './Loader';
-
-interface ServiceConditionsProps {
-  services?: BE_FieldType;
-}
-
-export const ServiceConditions: React.FC<ServiceConditionsProps> = ({ services }) => {
-  if (!services) return null;
-  const flexRef = useRef(null);
-  const { data, isLoading } = useServiceList();
-
-  const serviceData = data?.find(service => service.id === services?.fields.value);
-
-  useEffect(() => {
-    if (flexRef.current && targetAction === '#Service%20Name') {
-      focusCard(flexRef.current);
-    }
-  }, [isactionTrigger]);
-  if (isLoading) {
-    return <Loader />;
-  }
-  return (
-    <Wrapper
-      id={safeEncodeUriComponent(FILTER_TYPE_LABEL.service)}
-      border={styleActionOnTarget}
-      ref={flexRef}
-    >
-      <ServiceIcon width={24} height={24} />
-      <Text variant="h1" as="h1">
-        <strong> Service </strong>
-        <span>{OPERATOR_LABEL[services?.type.split('service_')[1] as OperatorTypes]}</span>
-      </Text>
-      <Text fontSize="2xl">{serviceData?.name ?? services?.fields.value}</Text>
-    </Wrapper>
-  );
-};
+  //why is not my commit push not showinf in github
