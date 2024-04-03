0009c5cf02790ad97486f5772f58765dc9d2d6f9
AsheshRanaGurung <ashesh7272@gmail.com>
2024-04-03 15:18:42 +0545

    chore: strict mode on

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import './index.css'
 import  { Toaster } from 'react-hot-toast';
 import { ChakraProvider } from '@chakra-ui/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
+    <React.StrictMode>
     <ChakraProvider>
         <Toaster
             position="top-right" />
         <App />
     </ChakraProvider>
-    // </React.StrictMode>,
+     </React.StrictMode>,
 )
